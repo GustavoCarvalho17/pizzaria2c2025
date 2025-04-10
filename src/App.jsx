d901665075126109604bbd253e54d546a2900e72
GustavoCarvalho17 <rm94872@estudante.fieb.edu.br>
2025-04-10 10:28:57 +0000

    vercel alterações

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,43 @@
-import{ useState } from "react"
+import {useState} from "react"
 
 function App(){
 
-   const[nome, setNome] = useState('Messi')
+  const [nome,setNome] = useState('Ronaldo')
 
-    constFormulario = (props) => {
+  const Formulario = () => {
+    return (
+      <div>
+           <input
+      className="nome"
+      onChange={(e)=>{setNome(e.target.value)}}
+      name="nome"
+      type="text" />
+      <button
+      onClick={()=>{alert(nome)}}
+      className="botao">
+      CLIQUE AQUI 
+      </button>
+      </div>
+    )
+  }
 
-     return(
-        <div>
-         <input 
-         placeholder={props.sombra}
-       className= "nome"
-       onChange={(e)=>{setNome(e.target.value)}}
-       name="nome"
-       type="text"/>
-       <button
-       onClick={()=>{(props.mensagem != null)? alert(props.mensagem)
-           :alert("TEXTO PADRAO")}}
-       className="botao">
-       {(props.textoBotao != null) ? props.textoBotao : "CLIQUE AQUI"}
-       </button>
-        </div>
+  return(
 
-     );
-    }; 
+    <div>
+      <h3>pizzaria 2c</h3>
+      <Formulario/>
+      <Formulario/>
+      <Formulario/>
 
-  return (
-   
-   <div>
-      <h3>Pizzaria 2c</h3>
-     <Formulario  sombra="Digite seu nome..." textoBotao="ENVIAR NOME" />
-     <Formulario  sombra="Digite seu e-mail..." textoBotao="E-MAIL"/>
-     <Formulario  sombra="(11)90000-0000" textoBotao="CELULAR"/>
-     <Formulario  mensagem="Mensagem enviada pelo App" />
-   </div>
-   
+
+    
 
 
 
-  );
+    </div>
+  )
+
+
 }
 
-export default App
+export default App 
